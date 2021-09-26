f4c19115041f92767be8dee175c4d0184fce925e
unknown <ferdi.rajadiacho@gmail.com>
2021-09-26 19:36:03 +0700

    HelloWord.java

---
+++ Praktikum/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld { 
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Ferdiansyah")		;
+		System.out.println("NIM : D3IF6706213141")	;
+		System.out.println("kelas : 45-01")	;
+	}
+}
